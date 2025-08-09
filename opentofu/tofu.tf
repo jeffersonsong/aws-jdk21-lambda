05dffe9e9cbe6e722e803c11a0f06035827ebb72
Jefferson Song <jeffersonsong@yahoo.com>
2025-08-09 11:30:12 -0400

    Upgrade jars.

--- opentofu/tofu.tf
+++ opentofu/tofu.tf
@@ -39,7 +39,7 @@ resource "aws_iam_role_policy_attachment" "logging" {
 
 # Lambda Function and Packaging
 resource "aws_s3_bucket" "bucket" {
-  bucket = "test-jar-2-etYlvlYlSOPSDLKK"
+  bucket = "test-jar-2-etylvlylsopsdlkk"
 }
 
 resource "aws_s3_object" "lambda_jar" {
